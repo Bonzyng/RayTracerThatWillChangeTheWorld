3b54ba68d9b9fef7429c6b22c7bfbd0d98611ee3
ThaliaDeShalit <thaliapenguin@gmail.com>
2015-04-25 01:44:56 +0300

    started trimesh init

--- src/ex3/surfaces/TriangleMesh.java
+++ src/ex3/surfaces/TriangleMesh.java
@@ -1,5 +1,37 @@
 package ex3.surfaces;
 
+import java.util.ArrayList;
+import java.util.Map;
+
+import math.Point3D;
+import math.Ray;
+import math.Vec;
+
 public class TriangleMesh extends Surface{
+	public ArrayList<Triangle> mTriangles;
+
+	public void init(Map<String, String> attributes) {
+		mTriangles = new ArrayList<Triangle>();
+		
+		int i = 0;
+		String triangle = "tri" + i;
+		while (attributes.containsKey(triangle)) {
+			mTriangles.add(new Triangle(triangle));
+			i++;
+			triangle = "tri" + i;
+		}
+	}
+	
+	@Override
+	public Point3D intersect(Ray iRay) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Vec getNormalAtPoint(Point3D point) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
