a0064814d43b8681f262684e2f5ca0fa59dbef62
Nadav <nadavglickman@gmail.com>
2015-04-24 18:01:02 +0300

    added empty methods to TriMesh so there won't be an error

--- src/ex3/surfaces/TriangleMesh.java
+++ src/ex3/surfaces/TriangleMesh.java
@@ -1,5 +1,21 @@
 package ex3.surfaces;
 
+import math.Point3D;
+import math.Ray;
+import math.Vec;
+
 public class TriangleMesh extends Surface{
+
+	@Override
+	public Point3D intersect(Ray iRay) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Vec getNormalAtPoint(Point3D point) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
